cd6345fe93e49dff370e7169945ea95c40bcc51b
elakkiyaselvan777 <91819335+elakkiyaselvan777@users.noreply.github.com>
2024-10-26 18:07:01 +0530

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,13 @@
+const browserVersion="Chrome"
+
+function getBrowserVersion(){
+    if(browserVersion=="Chrome"){
+        var browserVersion=10
+        console.log("Chrome Browser Version: "+browserVersion)
+    }else{
+        let browserVersion=12
+        console.log("Chrome Browser Version: "+browserVersion)
+    }
+}
+
+getBrowserVersion("Chrome")
